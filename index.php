ea3a07fb88bdd1b548a3fe94d6af6b0b36fb4931
Eldin Kecap <eldinkecap@gmail.com>
2022-06-28 22:45:23 +0200

    first steps in PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = "Eldin";
+    $age = 23;
+    echo "My name is {$name} ";
+    echo "and my age is ".$age."<br>";
+    echo "<br> I live in tuzla";
+    ?>
+</body>
+</html>
